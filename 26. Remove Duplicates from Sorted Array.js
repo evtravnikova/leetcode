40f4ada96328ce43deda473e4bf22c521f6c44ce
Olena Travnikova <elena@travnikova.pro>
2022-09-12 22:34:07 +0200

    9. Palindrome Number

---
+++ 26. Remove Duplicates from Sorted Array.js
@@ -0,0 +1,14 @@
+var removeDuplicates = function(nums) {
+    for (let i=0; i<nums.length;) {
+       if (nums[i] === nums[i+1]) {
+            nums.splice(i, 1)
+       }
+       else {
+           i++
+       }
+    }
+    console.log(nums)
+           return nums.length
+};
+
+removeDuplicates ([0,0,1,1,1,1,2,2,3,3,4,9])
